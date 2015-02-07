17123b3bd8a75b3a3e0fe08f150fb3c77b5381fe
Daniel Andrus <danieleandrus@gmail.com>
2015-02-07 14:29:55 -0700

    Added hello world program

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+	t.window.width = 400
+	t.window.height = 300
+	t.console = true
+end
---
+++ main.lua
@@ -0,0 +1,4 @@
+function love.draw()
+	love.graphics.print('Hello, World!', 200, 150)
+	print('Hello, World!')
+end
