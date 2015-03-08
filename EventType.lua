bde1546db0a3cb26f5e2c82e2c1ed107285e46eb
Daniel Andrus <danieleandrus@gmail.com>
2015-03-08 01:33:23 -0700

    Added core event functionality
    + Added error handling function catchError to functions.lua
    + Added callback executing function executeCallbacks to Secretary that
    executes all registered callbacks for the given EventType, catching
    errors and displaying them to the console
    + Added KEYBOARD_UP and KEYBOARD_DOWN event handling to Secretary (still
    need to be called from main.lua)
    * Few minor improvements to comments and code structure in Secretary.lua
    + Added more EventTypes

--- EventType.lua
+++ EventType.lua
@@ -8,17 +8,22 @@ EventType = {
   KEYBOARD_UP = 5,
   MOUSE_DOWN = 6,
   MOUSE_UP = 7,
+  MOUSE_MOVE = 8,
+  JOYSTICK_DOWN = 9,
+  JOYSTICK_UP = 10,
+  JOYSTICK_ADD = 11,
+  JOYSTICK_REMOVE = 12,
   
-  values = function()
+  values = function( )
     local i = 0
     return function()
       i = i + 1
-      if i <= 7 then return i end
+      if i <= 12 then return i end
     end
   end,
   
-  fromId = function(id)
-    if id > 7 or id < 1 then
+  fromId = function( id )
+    if id > 12 or id < 1 then
       return nil
     else
       return id
--- Secretary.lua
+++ Secretary.lua
@@ -1,7 +1,8 @@
+require "functions"
 require "QuadTree"
+require "EventType"
 
 -- Required fluff for classes
-require "functions"
 Secretary = {}
 Secretary.__index = Secretary
 
@@ -15,22 +16,24 @@ setmetatable(Secretary, {
   }
 )
 
+
 Secretary.objects = {}
 Secretary.nextId = 1
 Secretary.tree = QuadTree( 1, -1000, 1000, -1000, 1000 )
 Secretary.objectNodes = {}
 
+-- Set up callback tables for fast event lookups
+Secretary.callbacks = {}
+for t in EventType.values() do Secretary.callbacks[t] = {} end
+
+
 function Secretary.registerObject(object)
   
-  --Check that object is an instance of PhysObject
-  if instanceOf( object, PhysObject ) == false then
-    return false
-  end
+  -- Validate arguments
+  assert (instanceOf(object, PhysObject), "Argument must be an instance of PhysObject")
   
-  -- Make sure object is valid and ID is unset
-  if object == nil or object.id ~= nil then
-    return false
-  end
+  -- Make sure object ID is unset
+  assert (object.id == nil, "Object already assigned an ID")
   
   -- Assign object an ID, add to data structures
   object.id = Secretary.nextId
@@ -45,6 +48,28 @@ function Secretary.registerObject(object)
   return true
 end
 
+
+function Secretary.registerEvent(object, eventType, callback)
+  assert (object ~= nil, "Argument(s) cannot be nil")
+  assert (eventType ~= nil, "Argument(s) cannot be nil")
+  assert (callback ~= nil, "Argument(s) cannot be nil")
+  
+  local etype = EventType.fromId(etype)
+  assert (etype ~= nil, "eventType must be a valid EventType")
+  
+  assert (type(object.getInstanceId) ~= "function", "Object missing getInstanceId() method")
+  
+  local id = object:getInstanceId()
+  assert (type(id) == "number", "Object ID must be of type number")
+  assert (Secretary.objects[id] == object, "Object not registered with Secretary")
+  
+  assert (type(callback) == "function", "Callback must be a function")
+  assert (Secregary.callbacks[etype][id] == nil, "Event type already registered for that object")
+  
+  Secretary.callbacks[etype][id] = callback
+end
+
+
 function Secretary.updateObject(object)
   local path = Secretary.tree:getFullIndex(object:getBoundingBox())
   
@@ -54,6 +79,7 @@ function Secretary.updateObject(object)
     Secretary.objectNodes[object.id] = path
   end
 end
+
 
 function Secretary.getCollisions( top, right, bottom, left )
   local list = {}
@@ -71,14 +97,18 @@ function Secretary.getCollisions( top, right, bottom, left )
   return list
 end
 
+
 function Secretary.getObject( id )
   return Secretary.objects[id]
 end
 
+
 function Secretary.remove( object )
+  -- Validate arguments
   assert( type(object) == "number" or instanceOf( object, PhysObject ), "Invalid parameter type" )
   local id = object
   
+  -- Get ID if given object or object if given ID
   if type(id) == "number" then
     object = Secretary.objects[id]
     assert( object , "Invalid object ID "..id ) 
@@ -86,9 +116,15 @@ function Secretary.remove( object )
     id = object:getInstanceId()
   end
   
+  -- Remove object from tree and object lists
   Secretary.tree:remove(object, Secretary.objectNodes[id])
   Secretary.objects[id] = nil
   Secretary.objectNodes[id] = nil
+  
+  -- Remove object's registered event callback functions
+  for t in EventType.values() do
+    Secretary.callbacks[t][id] = nil
+  end
 end
 
 function Secretary.drawObjects( )
@@ -109,3 +145,37 @@ function Secretary.checkCollisions( )
   end
 end
 
+
+function Secretary.onKeyboardDown( key, isrepeat )
+  Secretary.executeCallbacks(EventType.KEYBOARD_DOWN, key, isrepeat)
+end
+
+
+function Secretary.onKeyboardUp( key )
+  Secretary.executeCallbacks(EventType.KEYBOARD_UP, key )
+end
+
+
+function Secretary.executeCallbacks( eventType, ... )
+  assert(EventType.fromId(eventType), "Invalid event type: "..eventType)
+  
+  -- Execute all registered callbacks registered with Secretary
+  for i,callback in pairs(Secretary.callbacks[eventType]) do
+    
+    -- Double-check that callback is real
+    if callback then
+      
+      -- Use xpcall to prevent errors from destroying everything
+      local success, errmessage = xpcall(
+        function() return callback(unpack(arg)) end,
+        catchError    -- function to handle errors
+      )
+      
+      -- If error occured, display traceback and continue
+      if success == false then
+        print("Error occured while handling event (type: "..eventType..")")
+        print("Caused by:", errmessage)
+      end
+    end
+  end
+end
