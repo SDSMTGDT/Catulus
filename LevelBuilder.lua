085014fade2b1c90822dde9ca54f4eebe85d9e7a
FrustyLove <jared.t.johnson@mines.sdsmt.edu>
2015-09-14 18:10:48 -0600

    Block Concatenation
    Doesn't work, will need to look at later. Commented out for now.

--- LevelBuilder.lua
+++ LevelBuilder.lua
@@ -34,9 +34,51 @@ function buildLevelFromFile(filename)
         -- Do nothing, blank space
         
       elseif (map[i][j] == "B") then
-        
+	  --[[
+        local k = i
+		local v = j
+		local firstPass = true
+		local checkWidth = 0
+		local bWidth = 0
+		local bHeight = 0
+		-- Loop
+		while map[k][j] == "B" and k < height do
+		  if checkWidth == bWidth then
+		    bHeight = bHeight + 16
+			if firstPass == false then
+			  while map[k-1][v] == "B" do
+			    map[k-1][v] = "b"
+			    v = v + 1
+			  end
+			end
+			v = j
+		  else
+			bWidth = 0
+			bHeight = 0
+			firstPass = true
+			--k = height
+		  end
+		  
+		  checkWidth = 0
+		  
+		  while map[k][v] == "B" and k ~= height do 
+		    if firstPass == true then
+			  bWidth = bWidth + 16
+			  map[k][v] = "b"
+			end
+			
+			checkWidth = checkWidth + 16
+			v = v + 1
+		  end
+		  
+		  firstPass = false
+		  v = j
+		  k = k + 1
+		end
+	  ]]--
+	  
         Block( (j-1) * 16, (i-1) * 16, 16, 16 )
-        
+		
       elseif (map[i][j] == "P") then
         
         local player = Player()
