9272b8d1b9178af887385f43f6cf9b054b051f9c
Deaboy <danieleandrus@gmail.com>
2015-12-30 20:41:41 -0700

    Greatly improved enumeration stuff

---
+++ DrawLayer.lua
@@ -0,0 +1,15 @@
+require "enum"
+
+-- Enum
+-- Defines depth for drawing layers
+DrawLayer = {
+  "BACKGROUND"        -- wallpapers, scenery
+  "BACKGROUND_PROPS"  -- props, behind actors
+  "MAIN"              -- where all the action happens
+  "SPOTLIGHT"         -- only for the most important actors
+  "FOREGROUND_PROPS"  -- in-your-face props
+  "FOREGROUND"        -- more decoration
+  "UI"                -- only the most important things
+}
+
+buildEnum(DrawLayer)
--- EventType.lua
+++ EventType.lua
@@ -1,36 +1,24 @@
+require "enum"
+
 -- Enum
 -- Event types; used for event registration
 EventType = {
-  DRAW = 1,
-  STEP = 2,
-  PRE_PHYSICS = 3,
-  PHYSICS = 4,
-  POST_PHYSICS = 5,
-  KEYBOARD_DOWN = 6,
-  KEYBOARD_UP = 7,
-  MOUSE_DOWN = 8,
-  MOUSE_UP = 9,
-  MOUSE_MOVE = 10,
-  JOYSTICK_DOWN = 11,
-  JOYSTICK_UP = 12,
-  JOYSTICK_ADD = 13,
-  JOYSTICK_REMOVE = 14,
-  WINDOW_RESIZE = 15,
-  PRE_DRAW = 16,
-  
-  values = function( )
-    local i = 0
-    return function()
-      i = i + 1
-      if i <= 16 then return i end
-    end
-  end,
-  
-  fromId = function( id )
-    if id > 16 or id < 1 then
-      return nil
-    else
-      return id
-    end
-  end,
+  "DRAW",
+  "STEP",
+  "PRE_PHYSICS",
+  "PHYSICS",
+  "POST_PHYSICS",
+  "KEYBOARD_DOWN",
+  "KEYBOARD_UP",
+  "MOUSE_DOWN",
+  "MOUSE_UP",
+  "MOUSE_MOVE",
+  "JOYSTICK_DOWN",
+  "JOYSTICK_UP",
+  "JOYSTICK_ADD",
+  "JOYSTICK_REMOVE",
+  "WINDOW_RESIZE",
+  "PRE_DRAW"
 }
+
+buildEnum(EventType)
