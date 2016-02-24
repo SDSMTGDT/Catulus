06dc2c45c7843d8ef67ec3cfa117bbc70af41a37
Daniel Andrus <danieleandrus@gmail.com>
2016-02-23 22:26:21 -0700

    Added Entity class

---
+++ Entity.lua
@@ -0,0 +1,47 @@
+require "common/class"
+require "Secretary"
+
+Entity = buildClass()
+
+--------------------------------------------------------------------------------
+--                                Entity Class                                --
+--------------------------------------------------------------------------------
+
+function Entity:_init()
+  
+  -- Assigned secretary object
+  self.secretary = nil
+end
+
+function Entity:registerWithSecretary(secretary)
+  
+  -- Ensure secretary is of correct type
+  assertType(secretary, "secretary", Secretary)
+  
+  -- Make sure we're not already registered with a secretary
+  if self.secretary ~= nil then
+    self:deregisterWithSecretary()
+  end
+  
+  -- Store reference to secretary we are registered with
+  self.secretary = secretary
+end
+
+function Entity:deregisterWithSecretary()
+  
+  -- Remove all record of self from current secretary
+  if self.secretary ~= nil then
+    self.secretary:remove(self)
+  end
+  
+  -- Forget registered secretary
+  self.secretary = nil
+end
+
+function Entity:getSecretary()
+  return self.secretary
+end
+
+function Entity:destroy()
+  self:deregisterWithSecretary()
+end
